d18a431b0fd92627970e57ba0876c0671ddf1251
정민우 소프트웍스 <sae1013@gmail.com>
2026-01-02 17:44:41 +0900

    구독회원에게 메일/문자발송세팅 (엑셀기반) 발송완료

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -66,7 +66,7 @@ export class OrdersService {
       this.configService.get('YOUTUBE_SPREAD_ID') ?? '',
       { range: '유튜브 프리미엄 인도계정!G2:L' },
     );
-
+    const targetUser: any[] = [];
     for (const row of response) {
       const [expireDate, useYn, contact, channel, etc, nickname] = row;
 
@@ -95,33 +95,35 @@ export class OrdersService {
           );
 
         const textMessage = `
-              안녕하세요. 유튜브 프리미엄 만료 전 안내 문자/메일 드립니다.\n만료: ${this.toKoreanMonthDay(expireDate)}\n(${r.reason === 'expired' ? '이미 만료' : '만료 임박'}) \n\n입금계좌: 3333060031134 \n 입금은행: 카카오뱅크 \n 계좌주: 정민우\n\n입금시 본인의 닉네임으로 입금해주시면 확인이 빠릅니다.\n본명으로 입금하실 경우 입금 후 별도로 알려주시면 감사드리겠습니다.\n좋은 하루 되세요~^^`.trim();
+              안녕하세요. 유튜브 프리미엄 만료 전 안내 문자/메일 드립니다.\n만료: ${this.toKoreanMonthDay(expireDate)}\n(${r.reason === 'expired' ? '이미 만료' : '만료 임박'}) \n\n입금계좌: 3333060031134 \n 입금은행: 카카오뱅크 \n 계좌주: 정민우\n\n입금시 본인의 닉네임으로 입금해주시면 확인이 빠릅니다.\n본명으로 입금하실 경우 입금 후 별도로 알려주시면 감사드리겠습니다.\n만료 7일이상 지난계정은 추가문자없이 종료처리하겠습니다. \n 좋은 하루 되세요~^^`.trim();
 
         const isSMS = contact.includes('@') ? 0 : 1;
 
+        targetUser.push(nickname);
         console.log(`${nickname}에게 발송`);
 
         const emailMessage = getEmailTemplate({
           expireDate: this.toKoreanMonthDay(expireDate),
           reason: r.reason === 'expired' ? '이미 만료' : '만료 임박',
         });
-
         /**
          * 대고객 문자/메일발송입니다. 아래 주석은 유의해주세요.
          */
-        // if (isSMS) {
-        //   const response = await this.sendSMS(contact, textMessage);
-        // } else {
-        //   const response = await this.gmailMailer.send({
-        //     to: contact,
-        //     subject: '유튜브 프리미엄 갱신안내',
-        //     html: emailMessage,
-        //     from: 'sae1013@gmail.com',
-        //   });
-        // }
+        if (isSMS) {
+          const response = await this.sendSMS(contact, textMessage);
+        } else {
+          const response = await this.gmailMailer.send({
+            to: contact,
+            subject: '유튜브 프리미엄 갱신안내',
+            html: emailMessage,
+            from: 'sae1013@gmail.com',
+          });
+        }
       } catch (err) {
         // 필요하면 err 로그
         console.log(err?.message ?? err);
+      } finally {
+        console.log(targetUser);
       }
     }
 
